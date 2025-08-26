b0618244ab268a46c6f3777b0650515a04f18df4
Nishant <59884657+Nishant0707@users.noreply.github.com>
2025-08-26 13:21:06 +0530

    Update Education.jsx

--- frontend/src/pages/Education.jsx
+++ frontend/src/pages/Education.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
-import SereduBanner from '../components/edu/SereduBanner';
-import SereduBlog from '../components/edu/SereduBlog';
+import SereduBanner from '../components/Edu/SereduBanner';
+import SereduBlog from '../components/Edu/SereduBlog';
 import SereduTypeEA from '../components/Edu/SereduTypeEA';
 import Sereduque from '../components/Edu/Sereduque';
 import Seredueplat from '../components/Edu/Seredueplat'
